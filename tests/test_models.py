fc044c1ea2f5f4fb1faac2c0d3cb934c0a690201
fionaromandemiguel <f.j.romandemiguel@vu.nl>
2024-10-08 12:37:15 +0200

    Refractored and tested all the code on branch full-data-analysis

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import numpy.testing as npt
+from unittest.mock import Mock
+import math
 
 
 def test_daily_mean_zeros():
@@ -29,3 +31,11 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_compute_data_mock_source():
+    from inflammation.compute_data import analyse_data
+    data_source = Mock()
+    data_source.load_inflammation_data.return_value = [[[0, 2, 0]],
+    [[0, 1, 0]]]
+    result = analyse_data(data_source)
+    npt.assert_array_almost_equal(result, [0, math.sqrt(0.25) ,0])
