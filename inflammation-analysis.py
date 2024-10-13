f189d0c4685f3a9736c91b308c7f37e4c6adb8db
fionaromandemiguel <f.j.romandemiguel@vu.nl>
2024-10-13 18:54:50 +0200

    code refactored and tested.

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -9,56 +9,41 @@ from inflammation.compute_data import analyse_data, CSVDataSource, JSONDataSourc
 
 
 def main(args):
+
     """The MVC Controller of the patient inflammation data system.
 
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    infiles = args.infiles
-    if not isinstance(infiles, list):
-        infiles = [args.infiles]
 
-    if args.full_data_analysis:
-        _, extension = os.path.splitext(infiles[0])
-    if extension == '.json':
-        data_source = JSONDataSource(os.path.dirname(infiles[0]))
-    elif extension == '.csv':
-        data_source = CSVDataSource(os.path.dirname(infiles[0]))
-    else:
-        raise ValueError(f'Unsupported file format: {extension}')
-    data_result = analyse_data(data_source)
-    graph_data = {
-        'standard deviation by day': data_result,
-    }
-    views.visualize(graph_data)
-    return
+    in_files = args.in_files
+    if not isinstance(in_files, list):
+        in_files = [args.in_files]
 
-    # for filename in infiles:
-    #     inflammation_data = models.load_csv(filename)
-    #
-    #     view_data = {
-    #         'average': models.daily_mean(inflammation_data),
-    #         'max': models.daily_max(inflammation_data),
-    #         'min': models.daily_min(inflammation_data)
-    #     }
-    #
-    #     views.visualize(view_data)
-
+    for filename in in_files:
+        # inflammation_data = models.load_csv(filename)
+        # data_source = CSVDataSource(os.path.dirname(filename))
+        if args.full_data_analysis:
+            _, extension = os.path.splitext(filename)
+            if extension == '.json':
+                data_source = JSONDataSource(os.path.dirname(filename))
+            elif extension == '.csv':
+                data_source = CSVDataSource(os.path.dirname(filename))
+            else:
+                raise ValueError(f'Unsupported file format: {extension}')
+            data_result = analyse_data(data_source)
+            graph_data = {
+                'standard deviation by day': data_result,
+            }
+            views.visualize(graph_data)
+            return
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='A basic patient inflammation data management system')
+    parser = argparse.ArgumentParser(description='A basic patient inflammation data management system')
 
     parser.add_argument(
-        'infiles',
-        nargs='+',
-        help='Input CSV(s) containing inflammation series for each patient')
-
-    parser.add_argument(
-        '--full-data-analysis',
-        action='store_true',
-        dest='full_data_analysis')
+        'in_files', nargs='+', help='Input CSV(s) containing inflammation series for each patient')
 
     args = parser.parse_args()
 
--- tests/test_compute_data.py
+++ tests/test_compute_data.py
@@ -2,6 +2,16 @@ import numpy.testing as npt
 from pathlib import Path
 import math
 import pytest
+from unittest.mock import Mock
+
+
+def test_compute_data_mock_source():
+    from inflammation.compute_data import analyse_data
+    data_source = Mock()
+    data_source.load_inflammation_data.return_value = [[[0, 2, 0]], [[0, 1, 0]]]
+    result = analyse_data(data_source)
+    npt.assert_array_almost_equal(result, [0, math.sqrt(0.25) ,0])
+
 
 def test_analyse_data():
 
