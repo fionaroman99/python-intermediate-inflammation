0d964ceaf025a01673572b344ecf05114f9cb0cb
fionaroman99 <f.j.romandemiguel@vu.nl>
2024-10-15 11:13:52 +0200

    Update inflammation-analysis.py

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -22,15 +22,14 @@ def main(args):
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-# <<<<<<< feature-std-dev
-#         view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data), **(models.s_dev(inflammation_data))}
-# =======
-#         view_data = {
-#             'average': models.daily_mean(inflammation_data), 
-#             'max': models.daily_max(inflammation_data),
-#             'min': models.daily_min(inflammation_data)}
-# >>>>>>> main
+        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 
+                     'min': models.daily_min(inflammation_data), **(models.s_dev(inflammation_data))}
 
+        view_data = {
+            'average': models.daily_mean(inflammation_data), 
+            'max': models.daily_max(inflammation_data),
+            'min': models.daily_min(inflammation_data)}
+        
         views.visualize(view_data)
 
 
